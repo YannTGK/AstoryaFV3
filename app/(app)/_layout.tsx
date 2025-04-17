03f514f0e0d2344a501040c6d5502385ed2c8180
HeliaDonad <r0936750@student.thomasmore.be>
2025-04-17 16:11:27 +0200

    feat: nav svg en stijl aanpassen

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,6 +1,23 @@
 import { Tabs, usePathname, useRouter } from "expo-router";
-import { Image, View, StyleSheet, TouchableOpacity, Text } from "react-native";
+import { View, StyleSheet, TouchableOpacity, Text } from "react-native";
 import { useState } from "react";
+
+import ExploreIcon from "@/assets/images/svg-icons/explore.svg";
+import ExploreActiveIcon from "@/assets/images/svg-icons/explore-active.svg";
+import DedicateIcon from "@/assets/images/svg-icons/dedicate.svg";
+import DedicateActiveIcon from "@/assets/images/svg-icons/dedicate-active.svg";
+import MyStarIcon from "@/assets/images/svg-icons/my-star.svg";
+import MyStarActiveIcon from "@/assets/images/svg-icons/my-star-active.svg";
+import AccountIcon from "@/assets/images/svg-icons/account.svg";
+import AccountActiveIcon from "@/assets/images/svg-icons/account-active.svg";
+import PublicIcon from "@/assets/images/svg-icons/public.svg";
+import PublicActiveIcon from "@/assets/images/svg-icons/public-active.svg";
+import PrivateIcon from "@/assets/images/svg-icons/private.svg";
+import PrivateActiveIcon from "@/assets/images/svg-icons/private-active.svg";
+import SearchIcon from "@/assets/images/svg-icons/search.svg";
+import SearchActiveIcon from "@/assets/images/svg-icons/search-active.svg";
+import CloseIcon from "@/assets/images/svg-icons/close.svg";
+import Close2Icon from "@/assets/images/svg-icons/close2.svg";
 
 export default function AppLayout() {
   const pathname = usePathname();
@@ -10,7 +27,7 @@ export default function AppLayout() {
 
   const handleTabPress = (tab: string) => {
     setActiveTab(tab);
-    router.replace(`/explores/${tab}`); // Navigate inside "explores"
+    router.replace(`/explores/${tab}`);
   };
 
   return (
@@ -22,161 +39,77 @@ export default function AppLayout() {
           tabBarLabelStyle: styles.tabLabel,
           tabBarActiveTintColor: "#273166",
           tabBarInactiveTintColor: "#C4C4C4",
+          tabBarIconStyle: styles.tabIcon,
         }}
       >
-       <Tabs.Screen
+        <Tabs.Screen
           name="explores"
           options={{
             tabBarLabel: "Explore",
-            tabBarIcon: ({ focused }) => (
-              <Image
-                source={
-                  focused
-                    ? require("@/assets/images/icons/explore-active.png")
-                    : require("@/assets/images/icons/explore.png")
-                }
-                style={styles.icon}
-              />
-            ),
+            tabBarIcon: ({ focused }) =>
+              focused ? <ExploreActiveIcon width={28} height={28} /> : <ExploreIcon width={28} height={28} />,
           }}
         />
         <Tabs.Screen
           name="dedicates"
           options={{
             tabBarLabel: "Dedicate",
-            tabBarIcon: ({ focused }) => (
-              <Image
-                source={
-                  focused
-                    ? require("@/assets/images/icons/dedicate-active.png")
-                    : require("@/assets/images/icons/dedicate.png")
-                }
-                style={styles.icon}
-              />
-            ),
+            tabBarIcon: ({ focused }) =>
+              focused ? <DedicateActiveIcon width={28} height={28} /> : <DedicateIcon width={28} height={28} />,
           }}
         />
         <Tabs.Screen
           name="my-stars"
           options={{
             tabBarLabel: "My Star",
-            tabBarIcon: ({ focused }) => (
-              <Image
-                source={
-                  focused
-                    ? require("@/assets/images/icons/my-star-active.png")
-                    : require("@/assets/images/icons/my-star.png")
-                }
-                style={styles.icon}
-              />
-            ),
+            tabBarIcon: ({ focused }) =>
+              focused ? <MyStarActiveIcon width={28} height={28} /> : <MyStarIcon width={28} height={28} />,
           }}
         />
         <Tabs.Screen
           name="accounts"
           options={{
             tabBarLabel: "Account",
-            tabBarIcon: ({ focused }) => (
-              <Image
-                source={
-                  focused
-                    ? require("@/assets/images/icons/account-active.png")
-                    : require("@/assets/images/icons/account.png")
-                }
-                style={styles.icon}
-              />
-            ),
+            tabBarIcon: ({ focused }) =>
+              focused ? <AccountActiveIcon width={26} height={26} /> : <AccountIcon width={26} height={26} />,
           }}
         />
       </Tabs>
 
-      {/* ✅ Show Custom Explore Menu Only When Explore is Active */}
       {pathname.includes("explores") && (
         <>
-          {/* Close button blijft altijd zichtbaar */}
           <View style={styles.exploreMenu}>
             <View style={styles.centered}>
-              <TouchableOpacity 
-                style={styles.closeButton} 
-                onPress={() => setIsMenuOpen(!isMenuOpen)}
-              >
-                <Image
-                  source={
-                    isMenuOpen
-                      ? require("@/assets/images/icons/close.png") // Afbeelding voor open toestand
-                      : require("@/assets/images/icons/open.png") // Afbeelding voor gesloten toestand
-                  }
-                  style={styles.navIcon}
-                />
+              <TouchableOpacity style={styles.closeButton} onPress={() => setIsMenuOpen(!isMenuOpen)}>
+                {isMenuOpen ? <CloseIcon width={60} height={60} /> : <Close2Icon width={60} height={60} />}
               </TouchableOpacity>
             </View>
           </View>
 
-          {/* Custom menu alleen tonen als isMenuOpen true is */}
           {isMenuOpen && (
             <>
-              {/* ✅ Circular Background */}
               <View style={styles.circleHolder}>
                 <View style={styles.circle} />
               </View>
 
-              {/* ✅ Public Button */}
               <View style={styles.publicHolder}>
-                <TouchableOpacity
-                  style={styles.publicPlacement}
-                  onPress={() => handleTabPress("public")}
-                >
-                  <Image
-                    source={
-                      activeTab === "public"
-                        ? require("@/assets/images/icons/public-active.png")
-                        : require("@/assets/images/icons/public.png")
-                    }
-                    style={styles.icon}
-                  />
-                  <Text style={[styles.labelAround, activeTab === "public" && styles.activeText]}>
-                    Public
-                  </Text>
+                <TouchableOpacity style={styles.publicPlacement} onPress={() => handleTabPress("public")}>
+                  {activeTab === "public" ? <PublicActiveIcon width={28} height={28} /> : <PublicIcon width={28} height={28} />}
+                  <Text style={[styles.labelAround, activeTab === "public" && styles.activeText]}>Public</Text>
                 </TouchableOpacity>
               </View>
 
-              {/* ✅ Private Button */}
               <View style={styles.publicHolder2}>
-                <TouchableOpacity
-                  style={styles.publicPlacement}
-                  onPress={() => handleTabPress("private")}
-                >
-                  <Image
-                    source={
-                      activeTab === "private"
-                        ? require("@/assets/images/icons/private-active.png")
-                        : require("@/assets/images/icons/private.png")
-                    }
-                    style={styles.icon}
-                  />
-                  <Text style={[styles.labelAround, activeTab === "private" && styles.activeText]}>
-                    Private
-                  </Text>
+                <TouchableOpacity style={styles.publicPlacement} onPress={() => handleTabPress("private")}>
+                  {activeTab === "private" ? <PrivateActiveIcon width={28} height={28} /> : <PrivateIcon width={28} height={28} />}
+                  <Text style={[styles.labelAround, activeTab === "private" && styles.activeText]}>Private</Text>
                 </TouchableOpacity>
               </View>
 
-              {/* ✅ Search Button */}
               <View style={styles.publicHolder3}>
-                <TouchableOpacity
-                  style={styles.publicPlacement}
-                  onPress={() => handleTabPress("search")}
-                >
-                  <Image
-                    source={
-                      activeTab === "search"
-                        ? require("@/assets/images/icons/search-active.png")
-                        : require("@/assets/images/icons/search.png")
-                    }
-                    style={styles.icon}
-                  />
-                  <Text style={[styles.labelAround, activeTab === "search" && styles.activeText]}>
-                    Search
-                  </Text>
+                <TouchableOpacity style={styles.publicPlacement} onPress={() => handleTabPress("search")}>
+                  {activeTab === "search" ? <SearchActiveIcon width={26} height={26} /> : <SearchIcon width={26} height={26} />}
+                  <Text style={[styles.labelAround, activeTab === "search" && styles.activeText]}>Search</Text>
                 </TouchableOpacity>
               </View>
             </>
@@ -191,8 +124,8 @@ const styles = StyleSheet.create({
   tabBar: {
     backgroundColor: "white",
     position: "absolute",
-    height: 90,
-    paddingBottom: 20,
+    height: 80,
+    paddingBottom: 10,
     paddingTop: 10,
     borderTopWidth: 0,
     shadowColor: "#000",
@@ -206,14 +139,14 @@ const styles = StyleSheet.create({
     fontSize: 12,
     fontFamily: "Alice-Regular",
   },
-  icon: {
+  tabIcon: {
     width: 28,
     height: 28,
-    resizeMode: "contain",
+    marginBottom: 6,
   },
   exploreMenu: {
     position: "absolute",
-    bottom: 60,
+    bottom: 50,
     width: "100%",
     alignItems: "center",
     zIndex: 11,
@@ -226,16 +159,12 @@ const styles = StyleSheet.create({
     borderRadius: 30,
     zIndex: 15,
   },
-  navIcon: {
-    width: 60,
-    height: 60,
-  },
   centered: {
     alignItems: "center",
   },
   circleHolder: {
     position: "absolute",
-    bottom: 0,
+    bottom: -10,
     width: "100%",
     alignItems: "center",
     zIndex: 9,
@@ -249,7 +178,7 @@ const styles = StyleSheet.create({
   },
   publicHolder: {
     position: "absolute",
-    bottom: 100,
+    bottom: 90,
     left: "50%",
     marginLeft: -64,
     alignItems: "center",
@@ -257,7 +186,7 @@ const styles = StyleSheet.create({
   },
   publicHolder2: {
     position: "absolute",
-    bottom: 130,
+    bottom: 120,
     left: "50%",
     marginLeft: -13,
     alignItems: "center",
@@ -265,7 +194,7 @@ const styles = StyleSheet.create({
   },
   publicHolder3: {
     position: "absolute",
-    bottom: 100,
+    bottom: 90,
     left: "50%",
     marginLeft: 39,
     alignItems: "center",
@@ -284,6 +213,6 @@ const styles = StyleSheet.create({
   },
   activeText: {
     color: "#FEEDB6",
-    fontWeight: "bold",
+    fontFamily: "Alice-Regular",
   },
 });
