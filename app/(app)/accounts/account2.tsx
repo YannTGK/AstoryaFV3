b4ee8011ba2615b0f7409a5d954cf4faa761e9f0
Sedaezt <sedaezzat@icloud.com>
2025-04-22 20:09:43 +0200

    feat: account Visibility

--- app/(app)/accounts/account2.tsx
+++ app/(app)/accounts/account2.tsx
@@ -50,7 +50,7 @@ export default function Account2Screen() {
 
       {/* Account-opties */}
       <Option icon={<AccountIcon width={22} height={22} />} label="My profile" onPress={() => router.push("/(app)/accounts/my-account/my-profile")} />
-      <Option icon={<VisibilityIcon width={22} height={22} />} label="Visibility options" />
+      <Option icon={<VisibilityIcon width={22} height={22} />} label="Visibility options" onPress={() => router.push("/(app)/accounts/my-account/visibility")} />
       <Option icon={<AddedAccountsIcon width={22} height={22} />} label="Added accounts" />
       <Option icon={<PaymentMethodsIcon width={22} height={22} />} label="Payment methods" />
       <Option icon={<EditPasswordIcon width={22} height={22} />} label="Edit password" />
