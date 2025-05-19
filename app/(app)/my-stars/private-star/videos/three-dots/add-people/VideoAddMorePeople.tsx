b388dab98f5b39c5c94477d9c5766cf459eb185d
Yann Tagakou <tagakouyann@gmail.com>
2025-05-19 17:52:20 +0200

    feat: all add people pages done for video

--- app/(app)/my-stars/private-star/videos/three-dots/add-people/VideoAddMorePeople.tsx
+++ app/(app)/my-stars/private-star/videos/three-dots/add-people/VideoAddMorePeople.tsx
@@ -102,7 +102,7 @@ export default function AddMorePeople() {
         setConfirm(false);
         router.push({
           pathname:
-            "/(app)/my-stars/private-star/photos/three-dots/add-people/AddPeoplePage",
+            "/(app)/my-stars/private-star/videos/three-dots/add-people/VideoAddPeoplePage",
           params: { albumId },        // ← meegeven
         });
       }, 1600);
@@ -113,7 +113,7 @@ export default function AddMorePeople() {
 
   /* ---------- invite knop ---------- */
   const invite = () =>
-    router.push("/(app)/my-stars/private-star/photos/three-dots/add-people/send-invitation-photos");
+    router.push("/(app)/my-stars/private-star/videos/three-dots/add-people/Videosend-invitation-photos");
 
   /* ==================== UI ==================== */
   return (
